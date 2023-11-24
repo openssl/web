93358fb0dbf940e2bf609552804db86dbc79a541
Tomas Vavra <tvavra@openssl.org>
2023-11-24 16:26:38 +0100

    CDN Preparation - Comments processed

--- bin/cvejsontohtml.py
+++ bin/cvejsontohtml.py
@@ -6,29 +6,32 @@ import json
 import codecs
 import os
 import re
-import xml.sax.saxutils as saxutils 
+import xml.sax.saxutils as saxutils
 from optparse import OptionParser
 import datetime
 import sys
 
 parser = OptionParser()
- 
-parser.add_option("-b","--base",help="major version to filter on",dest="base")
-parser.add_option("-i","--inputdirectory",help="directory of json files",dest="directory")
-(options,args) = parser.parse_args()
+
+parser.add_option("-b", "--base", help="major version to filter on", dest="base")
+parser.add_option("-i", "--inputdirectory", help="directory of json files", dest="directory")
+(options, args) = parser.parse_args()
+
 
 def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
     return [int(text) if text.isdigit() else text.lower()
-            for text in _nsre.split(s)]   
+            for text in _nsre.split(s)]
+
 
 def getbasefor(fixedin):
-    dotparts = re.search('^(\d)\.(\d)\.(\d)',fixedin)
+    dotparts = re.search('^(\d)\.(\d)\.(\d)', fixedin)
     if not dotparts:
         return None
-    if int(dotparts.group(1))<3:
+    if int(dotparts.group(1)) < 3:
         # Old style, the base is first 3 digits no letters
-        return dotparts.group(1)+"."+dotparts.group(2)+"."+dotparts.group(3)
-    return dotparts.group(1)+"."+dotparts.group(2)        
+        return dotparts.group(1) + "." + dotparts.group(2) + "." + dotparts.group(3)
+    return dotparts.group(1) + "." + dotparts.group(2)
+
 
 base = options.base or ""
 cves = []
@@ -38,11 +41,11 @@ allbase = []
 for x in os.listdir(options.directory or "./"):
     if x.endswith(".json"):
         try:
-            fd = codecs.open(options.directory+x,"r","utf-8")
+            fd = codecs.open(options.directory + x, "r", "utf-8")
             cve = json.load(fd)
             cves.append(cve)
         except:
-            print ("Ignoring due to error parsing: "+options.directory+x)
+            print("Ignoring due to error parsing: " + options.directory + x)
             continue
 
 # Filter on version 
@@ -53,10 +56,10 @@ for cve in cves:
     if "statements" in cve:
         for statement in cve["statements"]:
             if (statement["base"] in (options.base or "none")):
-                statements +="<p>"+statement["text"].strip()+"</p>"
+                statements += "<p>" + statement["text"].strip() + "</p>"
     if "disputed" in cve:
         for dispute in cve["disputed"]:
-            disputedcve[dispute["cve"]]=dispute
+            disputedcve[dispute["cve"]] = dispute
     if "containers" in cve:
         cna = cve["containers"]["cna"]
         cveid = cve["cveMetadata"]["cveId"]
@@ -66,24 +69,24 @@ for cve in cves:
             if fixedbase and fixedbase not in allbase:
                 allbase.append(fixedbase)
             if (fixedin.startswith(base)):
-                datepublic = cna["datePublic"]+"-"+cveid
-                entries[datepublic]=cve
+                datepublic = cna["datePublic"] + "-" + cveid
+                entries[datepublic] = cve
 
 allbase = sorted(allbase, reverse=True)
-           
+
 lastfixedv = ""
 productname = ""
 sections = []
 lastyear = ""
 allyears = []
 allissues = ""
-for k,cve in sorted(entries.items(), reverse=True):
+for k, cve in sorted(entries.items(), reverse=True):
     year = k.split('-')[0]
 
     if (lastyear != year):
         if (lastyear != ""):
             allissues += "</dl>";
-        allissues += "<h3><a name=\"y%s\">%s</a></h3>\n<dl>" %(year,year)            
+        allissues += "<h3><a name=\"y%s\">%s</a></h3>\n<dl>" % (year, year)
         allyears.append(year)
         lastyear = year
 
@@ -94,10 +97,10 @@ for k,cve in sorted(entries.items(), reverse=True):
     allissues += "<dt>"
     # CVE name
     if cve:
-        allissues += "<a href=\"https://www.cve.org/CVERecord?id=%s\" name=\"%s\">%s</a> " %(cveid,cveid,cveid)        
+        allissues += "<a href=\"https://www.cve.org/CVERecord?id=%s\" name=\"%s\">%s</a> " % (cveid, cveid, cveid)
 
-    # Advisory (use the title instead of openssl advisory)
-    title= "(OpenSSL Advisory)"
+        # Advisory (use the title instead of openssl advisory)
+    title = "(OpenSSL Advisory)"
     refs = ""
     if "title" in cna:
         title = cna['title']
@@ -107,32 +110,32 @@ for k,cve in sorted(entries.items(), reverse=True):
             if "vendor-advisory" in ref["tags"]:
                 url = ref["url"]
                 if url.startswith("https://www.openssl.org/news/"):
-                    url = url.replace("https://www.openssl.org/news/","")
-                refs = "<a href=\""+url+"\">"+title+"</a>"
-    allissues += " "+refs
+                    url = url.replace("https://www.openssl.org/news/", "")
+                refs = "<a href=\"" + url + "\">" + title + "</a>"
+    allissues += " " + refs
 
     # Impact
     for metric in cna["metrics"]:
         if "other" in metric["format"]:
             impact = metric["other"]["content"]["text"]
             if not "unknown" in impact:
-                 metric_url = metric["other"]["type"]
-                 if metric["other"]["type"].startswith("https://www.openssl.org/policies/"):
-                     metric_url = metric_url.replace("https://www.openssl.org/","../")
-                    
-                 allissues += f" <a href=\"{metric_url}\">[{impact} severity]</a>"
-            
+                metric_url = metric["other"]["type"]
+                if metric["other"]["type"].startswith("https://www.openssl.org/policies/"):
+                    metric_url = metric_url.replace("https://www.openssl.org/", "../")
+
+                allissues += f" <a href=\"{metric_url}\">[{impact} severity]</a>"
+
     # Date
-    datepublic =cna["datePublic"]
+    datepublic = cna["datePublic"]
     t = datetime.datetime(int(datepublic[:4]), int(datepublic[5:7]), int(datepublic[8:10]), 0, 0)
-    allissues += t.strftime(" %d %B %Y: ")    
+    allissues += t.strftime(" %d %B %Y: ")
 
     allissues += "<a href=\"#toc\"><img src=\"../img/up.gif\"/></a></dt>\n<dd>"
-    
+
     # Description
     for desc in cna["descriptions"]:
         # Trailing \n's are ignored, double \n are paragraph breaks
-        allissues += desc["value"].rstrip('\n').replace('\n\n',"</dd><dd>")
+        allissues += desc["value"].rstrip('\n').replace('\n\n', "</dd><dd>")
 
     # Credits
     if ("credits" in cna):
@@ -143,7 +146,7 @@ for k,cve in sorted(entries.items(), reverse=True):
                 creditprefix = " Fix developed by "
             elif "type" in credit and ("finder" not in credit["type"] and "reporter" not in credit["type"]):
                 creditprefix = " Thanks to "
-            allissues += creditprefix+credit["value"]+"."
+            allissues += creditprefix + credit["value"] + "."
 
     affects = []
     product = cna["affected"][0]
@@ -151,7 +154,7 @@ for k,cve in sorted(entries.items(), reverse=True):
     allissues += "<ul>"
     also = []
     for ver in product["versions"]:
-        if "lessThanOrEqual" in ver:   # It's not in a release yet!
+        if "lessThanOrEqual" in ver:  # It's not in a release yet!
             lastaffected = ver["lessThanOrEqual"]
             earliest = ver["version"]
             git = ""
@@ -161,59 +164,61 @@ for k,cve in sorted(entries.items(), reverse=True):
 
             if base:
                 if (not earliest.startswith(base)):
-                    also.append("OpenSSL <a href=\"vulnerabilities-%s.html#%s\">%s</a>" %( getbasefor(earliest), cveid, fixedin))
+                    also.append("OpenSSL <a href=\"vulnerabilities-%s.html#%s\">%s</a>" % (getbasefor(earliest), cveid, fixedin))
                     continue
-            allissues += "<li>Affects %s up to and including OpenSSL %s " %(earliest, lastaffected)
+            allissues += "<li>Affects %s up to and including OpenSSL %s " % (earliest, lastaffected)
             if (git != ""):
-                    issue = git.split('/')[-1]
-                    if git.startswith("https://git.openssl.org/"):
-                        commitId=git.split(";")[-1].split("=")[-1]
-                        git = f"https://github.com/openssl/openssl/commit/{commitId}"
-                    allissues += "<a href=\"%s\">(fix in git commit)</a> " %(git)
-            allissues += "</li>"            
+                issue = git.split('/')[-1]
+                # it will process this url https://git.openssl.org/gitweb/?p=openssl.git;a=commitdiff;h=517a0e7fa0f5453c860a3aec17b678bd55d5aad7
+                # and rewrite it to https://github.com/openssl/openssl/commit/517a0e7fa0f5453c860a3aec17b678bd55d5aad7
+                if git.startswith("https://git.openssl.org/"):
+                    commitId = git.split(";")[-1].split("=")[-1]
+                    git = f"https://github.com/openssl/openssl/commit/{commitId}"
+                allissues += "<a href=\"%s\">(fix in git commit)</a> " % (git)
+            allissues += "</li>"
         if "lessThan" in ver:
             fixedin = ver["lessThan"]
             earliest = ver["version"]
             git = ""
             for reference in cna["references"]:
-                if reference["name"].startswith(fixedin+" git"):
+                if reference["name"].startswith(fixedin + " git"):
                     git = reference["url"]
 
             if base:
                 if (not earliest.startswith(base)):
-                    also.append("OpenSSL <a href=\"vulnerabilities-%s.html#%s\">%s</a>" %( getbasefor(earliest), cveid, fixedin))
+                    also.append("OpenSSL <a href=\"vulnerabilities-%s.html#%s\">%s</a>" % (getbasefor(earliest), cveid, fixedin))
                     continue
-            allissues += "<li>Fixed in OpenSSL %s " %(fixedin)
+            allissues += "<li>Fixed in OpenSSL %s " % (fixedin)
             if (git != ""):
                 if git.startswith("https://git.openssl.org/"):
-                        commitId=git.split(";")[-1].split("=")[-1]
-                        git = f"https://github.com/openssl/openssl/commit/{commitId}"
-                if (fixedin.startswith("1.0.2") and fixedin[5]>='w'):  # 1.0.2w and above hack
-                    allissues += "<a href=\"/support/contracts.html?giturl=%s\">(premium support)</a> " %(git)
+                    commitId = git.split(";")[-1].split("=")[-1]
+                    git = f"https://github.com/openssl/openssl/commit/{commitId}"
+                if (fixedin.startswith("1.0.2") and fixedin[5] >= 'w'):  # 1.0.2w and above hack
+                    allissues += "<a href=\"/support/contracts.html?giturl=%s\">(premium support)</a> " % (git)
                 else:
-                    allissues += "<a href=\"%s\">(git commit)</a> " %(git)
-            allissues += "(Affected since "+earliest+")"       
+                    allissues += "<a href=\"%s\">(git commit)</a> " % (git)
+            allissues += "(Affected since " + earliest + ")"
             allissues += "</li>"
     if also:
-         allissues += "<li>This issue was also addressed in "+ ", ".join( also)
+        allissues += "<li>This issue was also addressed in " + ", ".join(also)
     allissues += "</ul></dd>\n"
 
 preface = "<!-- do not edit this file it is autogenerated, edit vulnerabilities.xml -->"
 bases = []
 for base in allbase:
     if (options.base and base in options.base):
-        bases.append("%s" %(base))
+        bases.append("%s" % (base))
     else:
-        bases.append( "<a href=\"vulnerabilities-%s.html\">%s</a>" %(base,base))
+        bases.append("<a href=\"vulnerabilities-%s.html\">%s</a>" % (base, base))
 preface += "<p>Show issues fixed only in OpenSSL " + ", ".join(bases)
 if options.base:
     preface += ", or <a href=\"vulnerabilities.html\">all versions</a></p>"
-    preface += "<h2>Fixed in OpenSSL %s</h2>" %(options.base)
+    preface += "<h2>Fixed in OpenSSL %s</h2>" % (options.base)
 else:
     preface += "</p>"
 preface += statements
-if len(allyears)>1: # If only vulns in this year no need for the year table of contents
-    preface += "<p><a name=\"toc\">Jump to year: </a>" + ", ".join( "<a href=\"#y%s\">%s</a>" %(year,year) for year in allyears)
+if len(allyears) > 1:  # If only vulns in this year no need for the year table of contents
+    preface += "<p><a name=\"toc\">Jump to year: </a>" + ", ".join("<a href=\"#y%s\">%s</a>" % (year, year) for year in allyears)
 preface += "</p>"
 if allissues != "":
     preface += allissues + "</dl>"
@@ -223,11 +228,11 @@ else:
 nonissues = ""
 for nonissue in disputedcve:
     if (not options.base or disputedcve[nonissue]["base"] in (options.base or "none")):
-        nonissues += "<li><a href=\"https://www.cve.org/CVERecord?id=%s\" name=\"%s\">%s</a>: " %(nonissue,nonissue,nonissue)        
+        nonissues += "<li><a href=\"https://www.cve.org/CVERecord?id=%s\" name=\"%s\">%s</a>: " % (nonissue, nonissue, nonissue)
         nonissues += disputedcve[nonissue]["text"]
-        nonissues +="</li>"
+        nonissues += "</li>"
 if (nonissues != ""):
-    preface += "<h3>Not Vulnerabilities</h3><ul>" + nonissues + "</ul>"    
+    preface += "<h3>Not Vulnerabilities</h3><ul>" + nonissues + "</ul>"
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stdout.write(preface)
--- bin/job_pool.sh
+++ bin/job_pool.sh
@@ -15,10 +15,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -51,104 +51,97 @@ job_pool_nerrors=0
 ################################################################################
 
 # \brief debug output
-function _job_pool_echo()
-{
-    if [[ "${job_pool_echo_command}" == "1" ]]; then
-        echo $@
-    fi
+function _job_pool_echo() {
+	if [[ ${job_pool_echo_command} == "1" ]]; then
+		echo $@
+	fi
 }
 
 # \brief cleans up
-function _job_pool_cleanup()
-{
-    rm -f ${job_pool_job_queue} ${job_pool_result_log}
+function _job_pool_cleanup() {
+	rm -f ${job_pool_job_queue} ${job_pool_result_log}
 }
 
 # \brief signal handler
-function _job_pool_exit_handler()
-{
-    _job_pool_stop_workers
-    _job_pool_cleanup
+function _job_pool_exit_handler() {
+	_job_pool_stop_workers
+	_job_pool_cleanup
 }
 
 # \brief print the exit codes for each command
 # \param[in] result_log  the file where the exit codes are written to
-function _job_pool_print_result_log()
-{
-    job_pool_nerrors=$(grep ^ERROR "${job_pool_result_log}" | wc -l)
-    cat "${job_pool_result_log}" | sed -e 's/^ERROR//'
+function _job_pool_print_result_log() {
+	job_pool_nerrors=$(grep ^ERROR "${job_pool_result_log}" | wc -l)
+	cat "${job_pool_result_log}" | sed -e 's/^ERROR//'
 }
 
 # \brief the worker function that is called when we fork off worker processes
 # \param[in] id  the worker ID
 # \param[in] job_queue  the fifo to read jobs from
 # \param[in] result_log  the temporary log file to write exit codes to
-function _job_pool_worker()
-{
-    local id=$1
-    local job_queue=$2
-    local result_log=$3
-    local cmd=
-    local args=
+function _job_pool_worker() {
+	local id=$1
+	local job_queue=$2
+	local result_log=$3
+	local cmd=
+	local args=
 
-    exec 7<> ${job_queue}
-    while [[ "${cmd}" != "${job_pool_end_of_jobs}" && -e "${job_queue}" ]]; do
-        # workers block on the exclusive lock to read the job queue
-        flock --exclusive 7
-        IFS=$'\v'
-        read cmd args <${job_queue}
-        set -- ${args}
-        unset IFS
-        flock --unlock 7
-        # the worker should exit if it sees the end-of-job marker or run the
-        # job otherwise and save its exit code to the result log.
-        if [[ "${cmd}" == "${job_pool_end_of_jobs}" ]]; then
-            # write it one more time for the next sibling so that everyone
-            # will know we are exiting.
-            echo "${cmd}" >&7
-        else
-            _job_pool_echo "### _job_pool_worker-${id}: ${cmd}"
-            # run the job
-            { ${cmd} "$@" ; }
-            # now check the exit code and prepend "ERROR" to the result log entry
-            # which we will use to count errors and then strip out later.
-            local result=$?
-            local status=
-            if [[ "${result}" != "0" ]]; then
-                status=ERROR
-            fi
-            # now write the error to the log, making sure multiple processes
-            # don't trample over each other.
-            exec 8<> ${result_log}
-            flock --exclusive 8
-            _job_pool_echo "${status}job_pool: exited ${result}: ${cmd} $@" >> ${result_log}
-            flock --unlock 8
-            exec 8>&-
-            _job_pool_echo "### _job_pool_worker-${id}: exited ${result}: ${cmd} $@"
-        fi
-    done
-    exec 7>&-
+	exec 7<>${job_queue}
+	while [[ ${cmd} != "${job_pool_end_of_jobs}" && -e ${job_queue} ]]; do
+		# workers block on the exclusive lock to read the job queue
+		flock --exclusive 7
+		IFS=$'\v'
+		read cmd args <${job_queue}
+		set -- ${args}
+		unset IFS
+		flock --unlock 7
+		# the worker should exit if it sees the end-of-job marker or run the
+		# job otherwise and save its exit code to the result log.
+		if [[ ${cmd} == "${job_pool_end_of_jobs}" ]]; then
+			# write it one more time for the next sibling so that everyone
+			# will know we are exiting.
+			echo "${cmd}" >&7
+		else
+			_job_pool_echo "### _job_pool_worker-${id}: ${cmd}"
+			# run the job
+			{ ${cmd} "$@"; }
+			# now check the exit code and prepend "ERROR" to the result log entry
+			# which we will use to count errors and then strip out later.
+			local result=$?
+			local status=
+			if [[ ${result} != "0" ]]; then
+				status=ERROR
+			fi
+			# now write the error to the log, making sure multiple processes
+			# don't trample over each other.
+			exec 8<>${result_log}
+			flock --exclusive 8
+			_job_pool_echo "${status}job_pool: exited ${result}: ${cmd} $@" >>${result_log}
+			flock --unlock 8
+			exec 8>&-
+			_job_pool_echo "### _job_pool_worker-${id}: exited ${result}: ${cmd} $@"
+		fi
+	done
+	exec 7>&-
 }
 
 # \brief sends message to worker processes to stop
-function _job_pool_stop_workers()
-{
-    # send message to workers to exit, and wait for them to stop before
-    # doing cleanup.
-    echo ${job_pool_end_of_jobs} >> ${job_pool_job_queue}
-    wait
+function _job_pool_stop_workers() {
+	# send message to workers to exit, and wait for them to stop before
+	# doing cleanup.
+	echo ${job_pool_end_of_jobs} >>${job_pool_job_queue}
+	wait
 }
 
 # \brief fork off the workers
 # \param[in] job_queue  the fifo used to send jobs to the workers
 # \param[in] result_log  the temporary log file to write exit codes to
-function _job_pool_start_workers()
-{
-    local job_queue=$1
-    local result_log=$2
-    for ((i=0; i<${job_pool_pool_size}; i++)); do
-        _job_pool_worker ${i} ${job_queue} ${result_log} &
-    done
+function _job_pool_start_workers() {
+	local job_queue=$1
+	local result_log=$2
+	for ((i = 0; i < job_pool_pool_size; i++)); do
+		_job_pool_worker ${i} ${job_queue} ${result_log} &
+	done
 }
 
 ################################################################################
@@ -158,49 +151,45 @@ function _job_pool_start_workers()
 # \brief initializes the job pool
 # \param[in] pool_size  number of parallel jobs allowed
 # \param[in] echo_command  1 to turn on echo, 0 to turn off
-function job_pool_init()
-{
-    local pool_size=$1
-    local echo_command=$2
+function job_pool_init() {
+	local pool_size=$1
+	local echo_command=$2
 
-    # set the global attibutes
-    job_pool_pool_size=${pool_size:=1}
-    job_pool_echo_command=${echo_command:=0}
+	# set the global attibutes
+	job_pool_pool_size=${pool_size:=1}
+	job_pool_echo_command=${echo_command:=0}
 
-    # create the fifo job queue and create the exit code log
-    rm -rf ${job_pool_job_queue} ${job_pool_result_log}
-    mkfifo ${job_pool_job_queue}
-    touch ${job_pool_result_log}
+	# create the fifo job queue and create the exit code log
+	rm -rf ${job_pool_job_queue} ${job_pool_result_log}
+	mkfifo ${job_pool_job_queue}
+	touch ${job_pool_result_log}
 
-    # fork off the workers
-    _job_pool_start_workers ${job_pool_job_queue} ${job_pool_result_log}
+	# fork off the workers
+	_job_pool_start_workers ${job_pool_job_queue} ${job_pool_result_log}
 }
 
 # \brief waits for all queued up jobs to complete and shuts down the job pool
-function job_pool_shutdown()
-{
-    _job_pool_stop_workers
-    _job_pool_print_result_log
-    _job_pool_cleanup
+function job_pool_shutdown() {
+	_job_pool_stop_workers
+	_job_pool_print_result_log
+	_job_pool_cleanup
 }
 
 # \brief run a job in the job pool
-function job_pool_run()
-{
-    if [[ "${job_pool_pool_size}" == "-1" ]]; then
-        job_pool_init
-    fi
-    printf "%s\v" "$@" >> ${job_pool_job_queue}
-    echo >> ${job_pool_job_queue}
+function job_pool_run() {
+	if [[ ${job_pool_pool_size} == "-1" ]]; then
+		job_pool_init
+	fi
+	printf "%s\v" "$@" >>${job_pool_job_queue}
+	echo >>${job_pool_job_queue}
 }
 
 # \brief waits for all queued up jobs to complete before starting new jobs
 # This function actually fakes a wait by telling the workers to exit
 # when done with the jobs and then restarting them.
-function job_pool_wait()
-{
-    _job_pool_stop_workers
-    _job_pool_start_workers ${job_pool_job_queue} ${job_pool_result_log}
+function job_pool_wait() {
+	_job_pool_stop_workers
+	_job_pool_start_workers ${job_pool_job_queue} ${job_pool_result_log}
 }
 #########################################
 # End of Job Pool
